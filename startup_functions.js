c601686173814234aca016e9b40b0227659e18ce
Byron <byron.nill10@gmail.com>
2020-02-23 19:46:16 +0800

    anim startup, needs reg validation

--- startup_functions.js
+++ startup_functions.js
@@ -1,12 +1,40 @@
-// incomplete function
+$("#email_login, #password_login").on("keypress", function() {
+    let holder1 = $("#email_login").val().length;
+    let holder2 = $("#password_login").val().length;
 
-function detectCompleteEntries() {
-
-    let content1 = $.trim(document.getElementById("email_login").value).length;
-    let content2 = $.trim(document.getElementById("password_login").value).length;
-
-    if (content1 !== 0 && content2 !== 0) {
-        $("#submit_login").disabled = false;
+    if (holder1 !== 0 && holder2 !== 0) {
+        $("#submit_login").removeAttr("disabled");
     }
+});
 
-}
+$("#email_login, #password_login").on("focus", function () {
+
+    $("#login_box").removeClass();
+    $("#login_box").addClass("col-sm-8");
+
+    $("#register_box").removeClass();
+    $("#register_box").addClass("col-sm-4");
+
+});
+
+$("#fname, #lname, #bday, #gender, #email_register, #password_register, #cpass").on("focus", function () {
+
+    $("#login_box").removeClass();
+    $("#login_box").addClass("col-sm-4");
+
+    $("#register_box").removeClass();
+    $("#register_box").addClass("col-sm-8");
+
+});
+
+$("#email_login, #password_login, #fname, #lname, #bday, #gender, #email_register, #password_register, #cpass").on("blur", function () {
+
+    let holder1
+
+    $("#login_box").removeClass();
+    $("#login_box").addClass("col-sm-5");
+
+    $("#register_box").removeClass();
+    $("#register_box").addClass("col-sm-7");
+
+});
