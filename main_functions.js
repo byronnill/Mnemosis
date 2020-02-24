041ea45489f3ea4a0fb38033a2904ddfb1e18984
Byron <byron.nill10@gmail.com>
2020-02-24 14:52:32 +0800

    feb 24 build

--- main_functions.js
+++ main_functions.js
@@ -2,7 +2,7 @@ $(window).on("scroll", function () {
 
     let height = $(window).scrollTop();
 
-    if (height > 200) {
+    if (height > 0) {
         $("#web_title").css("color","blue");
     } else {
         $("#web_title").css("color","black");
