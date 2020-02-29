117f9f43d66248fa86a700c55c4f8e07b42bea8e
Byron <byron.nill10@gmail.com>
2020-02-29 18:27:15 +0800

    fixed reg valdiation

--- js/login_and_register.js
+++ js/login_and_register.js
@@ -10,13 +10,21 @@ $("#email-login, #password-login").on("keyup", function() {
 });
 
 $("#fname, #lname, #bday, #gender, #email-register, #password-register, #cpass").on("keyup", function() {
-    let holder1 = $("#email-login").val().length;
-    let holder2 = $("#password-login").val().length;
+    let valid = []
 
-    if (holder1 !== 0 && holder2 !== 0 && $("#email-login").is(":valid")) {
-        $("#submit-login").prop("disabled", false);
+    valid.push($("#fname").val().length > 0);
+    valid.push($("#lname").val().length > 0);
+    valid.push($("#bday").val().length > 0);
+    valid.push($("#gender").val() !== "Your gender");
+    valid.push($("#email-register").val().length > 0 && $("#email-register").is(":valid"));
+    valid.push($("#password-register").val().length > 0);
+    valid.push($("#cpass").val().length > 0);
+    valid.push($("#password-register").val() === $("#cpass").val());
+
+    if (valid.indexOf(false) === -1) {
+        $("#submit-register").prop("disabled", false);
     } else {
-        $("#submit-login").prop("disabled", true);
+        $("#submit-register").prop("disabled", true);
     }
 });
 
@@ -41,8 +49,6 @@ $("#fname, #lname, #bday, #gender, #email-register, #password-register, #cpass")
 });
 
 $("#email-login, #password-login, #fname, #lname, #bday, #gender, #email-register, #password-register, #cpass").on("blur", function () {
-
-    let holder1
 
     $("#login-box").removeClass();
     $("#login-box").addClass("col-sm-5");
