b6f5dd72a64024a259dcb9e0fbb652e5977e97e8
Byron <byron.nill10@gmail.com>
2020-03-22 19:33:19 +0800

    separated routing

--- index.js
+++ index.js
@@ -5,57 +5,32 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
-app.get("/create_game", function(req, res) {
-    res.render("pages/create_game");
-});
+const createRoute = require("./controller/create_game");
+const homeRoute = require("./controller/homepage");
+const loginRoute = require("./controller/login_and_register");
+const modifyRoute = require("./controller/modify_game");
+const playRoute = require("./controller/play_game");
+const viewGamesRoute = require("./controller/view_games");
+const viewGamesAdminRoute = require("./controller/view_games_admin");
+const viewLeaderboard = require("./controller/view_leaderboard");
+const viewProfileAdminRoute = require("./controller/view_profile_admin");
+const viewProfileAdminUserRoute = require("./controller/view_profile_admin_user");
+const viewProfileSelf = require("./controller/view_profile_self");
+const viewProfileUserRoute = require("./controller/view_profile_user");
 
-app.get("/homepage", function(req, res) {
-    res.render("pages/homepage");
-});
-
-app.get("/", function(req, res) {
-    res.render("pages/login_and_register");
-});
-
-app.get("/login_and_register", function(req, res) {
-    res.render("pages/login_and_register");
-});
-
-app.get("/modify_game", function(req, res) {
-    res.render("pages/modify_game");
-});
-
-app.get("/play_game", function(req, res) {
-    res.render("pages/play_game");
-});
-
-app.get("/view_games", function(req, res) {
-    res.render("pages/view_games");
-});
-
-app.get("/view_games_admin", function(req, res) {
-    res.render("pages/view_games_admin");
-});
-
-app.get("/view_leaderboard", function(req, res) {
-    res.render("pages/view_leaderboard");
-});
-
-app.get("/view_profile_admin", function(req, res) {
-    res.render("pages/view_profile_admin");
-});
-
-app.get("/view_profile_admin_user", function(req, res) {
-    res.render("pages/view_profile_admin_user");
-});
-
-app.get("/view_profile_self", function(req, res) {
-    res.render("pages/view_profile_self");
-});
-
-app.get("/view_profile_user", function(req, res) {
-    res.render("pages/view_profile_user");
-});
+app.use("/create_game", createRoute);
+app.use("/homepage", homeRoute);
+app.use("/login_and_register", loginRoute);
+app.use("/", loginRoute);
+app.use("/modify_game", modifyRoute);
+app.use("/play_game", playRoute);
+app.use("/view_games", viewGamesRoute);
+app.use("/view_games_admin", viewGamesAdminRoute);
+app.use("/view_leaderboard", viewLeaderboard);
+app.use("/view_profile_admin", viewProfileAdminRoute);
+app.use("/view_profile_admin_user", viewProfileAdminUserRoute);
+app.use("/view_profile_self", viewProfileSelf);
+app.use("/view_profile_user", viewProfileUserRoute);
 
 app.listen(port, function() {
     console.log("listening at http://localhost:" + port);
