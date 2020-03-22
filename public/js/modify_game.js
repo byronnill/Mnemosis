82281dc7fbde30b8092be7720710a782158ec3f4
Byron <byron.nill10@gmail.com>
2020-03-22 16:49:19 +0800

    added genre selector in create/modify

--- public/js/modify_game.js
+++ public/js/modify_game.js
@@ -23,3 +23,14 @@ $("#exit-alert-stay").on("click", function () {
     $("#exit-alert").css("display", "none");
     $("#exit-alert-link").attr("href", "#");
 });
+
+$("#filter-by-art, #filter-by-business, #filter-by-scitech, #filter-by-general, #filter-by-trivia, #filter-by-sports, #filter-by-others").on("click", function () {
+    let bool = $(this).hasClass("active");
+    console.log(bool);
+
+    if (bool === true) {
+        $(this).removeClass("active");
+    } else {
+        $(this).addClass("active");
+    }
+});
