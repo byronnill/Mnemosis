aca7b1d3d76a8ccbef5359497e8116871cbe3f81
Byron <byron.nill10@gmail.com>
2020-03-23 15:32:14 +0800

    introduced partials, to place ptls on all docs

--- public/js/view_games.js
+++ public/js/view_games.js
@@ -31,6 +31,43 @@ search.on("click", function () {
     }
 });
 
+function check () {
+
+    $(".card").parent().hide();
+    none.removeClass("active");
+
+    if (art.hasClass("active"))
+        $(".container").find(".card-art").parent().show();
+    if (business.hasClass("active"))
+        $(".container").find(".card-business").parent().show();
+    if (scitech.hasClass("active"))
+        $(".container").find(".card-scitech").parent().show();
+    if (general.hasClass("active"))
+        $(".container").find(".card-general").parent().show();
+    if (trivia.hasClass("active"))
+        $(".container").find(".card-trivia").parent().show();
+    if (sports.hasClass("active"))
+        $(".container").find(".card-sports").parent().show();
+    if (others.hasClass("active"))
+        $(".container").find(".card-others").parent().show();
+
+    if ((art.hasClass("active") && business.hasClass("active") && scitech.hasClass("active") && general.hasClass("active") && trivia.hasClass("active") && sports.hasClass("active") && others.hasClass("active")) ||
+        !(art.hasClass("active") || business.hasClass("active") || scitech.hasClass("active") || general.hasClass("active") || trivia.hasClass("active") || sports.hasClass("active") || others.hasClass("active"))) {
+        none.addClass("active");
+
+        art.removeClass("active");
+        business.removeClass("active");
+        scitech.removeClass("active");
+        general.removeClass("active");
+        trivia.removeClass("active");
+        sports.removeClass("active");
+        others.removeClass("active");
+
+        $(".card").parent().show();
+    }
+
+}
+
 none.on("click", function () {
     none.addClass("active");
 
@@ -45,203 +82,70 @@ none.on("click", function () {
     $(".card").parent().show();
 });
 
-function check () {
-    let arr = [art, business, scitech, general, trivia, sports, others];
-    let active = [];
-
-    arr.forEach(function (i) {
-        if (i.hasClass("active"))
-            active.push(i);
-    });
-
-    if (active.length === 0) {
-        none.addClass("active");
-        $(".card").parent().show();
-    } else {
-        if (active.indexOf(art) !== -1)
-            $(".container").find(".card-art").parent().show();
-        if (active.indexOf(business) !== -1)
-            $(".container").find(".card-business").parent().show();
-        if (active.indexOf(scitech) !== -1)
-            $(".container").find(".card-scitech").parent().show();
-        if (active.indexOf(general) !== -1)
-            $(".container").find(".card-general").parent().show();
-        if (active.indexOf(trivia) !== -1)
-            $(".container").find(".card-trivia").parent().show();
-        if (active.indexOf(sports) !== -1)
-            $(".container").find(".card-sports").parent().show();
-        if (active.indexOf(others) !== -1)
-            $(".container").find(".card-others").parent().show();
-    }
-
-}
-
-$("#filter-by-art, #filter-by-business, #filter-by-scitech, #filter-by-general, #filter-by-trivia, #filter-by-sports, #filter-by-others").on("click", function () {
-    if (none.hasClass("active")) {
-        $(".card").parent().css("display", "none");
-        none.removeClass("active");
-    }
-});
 
 art.on("click", function () {
     if (art.hasClass("active")) {
         art.removeClass("active");
-        $(".container").find(".card-art").parent().css("display", "none");
-        check();
     } else {
         art.addClass("active");
-
-        $(".container").find(".card-art").parent().show();
-        if (business.hasClass("active") && scitech.hasClass("active") && general.hasClass("active") && trivia.hasClass("active") && sports.hasClass("active") && others.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
 
 business.on("click", function () {
     if (business.hasClass("active")) {
         business.removeClass("active");
-        $(".container").find(".card-business").parent().css("display", "none");
-        check();
     } else {
         business.addClass("active");
-
-        $(".container").find(".card-business").parent().show();
-        if (art.hasClass("active") && scitech.hasClass("active") && general.hasClass("active") && trivia.hasClass("active") && sports.hasClass("active") && others.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
 
 scitech.on("click", function () {
     if (scitech.hasClass("active")) {
         scitech.removeClass("active");
-        $(".container").find(".card-scitech").parent().css("display", "none");
-        check();
     } else {
         scitech.addClass("active");
-
-        $(".container").find(".card-scitech").parent().show();
-        if (art.hasClass("active") && business.hasClass("active") && general.hasClass("active") && trivia.hasClass("active") && sports.hasClass("active") && others.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
+
 
 general.on("click", function () {
     if (general.hasClass("active")) {
         general.removeClass("active");
-        $(".container").find(".card-general").parent().css("display", "none");
-        check();
     } else {
         general.addClass("active");
-
-        $(".container").find(".card-general").parent().show();
-        if (art.hasClass("active") && business.hasClass("active") && scitech.hasClass("active") && trivia.hasClass("active") && sports.hasClass("active") && others.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
 
 trivia.on("click", function () {
     if (trivia.hasClass("active")) {
         trivia.removeClass("active");
-        $(".container").find(".card-trivia").parent().css("display", "none");
-        check();
     } else {
         trivia.addClass("active");
-
-        $(".container").find(".card-trivia").parent().show();
-        if (art.hasClass("active") && business.hasClass("active") && scitech.hasClass("active") && general.hasClass("active") && sports.hasClass("active") && others.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
 
 sports.on("click", function () {
     if (sports.hasClass("active")) {
         sports.removeClass("active");
-        $(".container").find(".card-sports").parent().css("display", "none");
-        check();
     } else {
         sports.addClass("active");
-
-        $(".container").find(".card-sports").parent().show();
-        if (art.hasClass("active") && business.hasClass("active") && scitech.hasClass("active") && general.hasClass("active") && trivia.hasClass("active") && others.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
+
 
 others.on("click", function () {
     if (others.hasClass("active")) {
         others.removeClass("active");
-        $(".container").find(".card-others").parent().css("display", "none");
-        check();
     } else {
         others.addClass("active");
-
-        $(".container").find(".card-others").parent().show();
-        if (art.hasClass("active") && business.hasClass("active") && scitech.hasClass("active") && general.hasClass("active") && trivia.hasClass("active") && sports.hasClass("active")) {
-            none.addClass("active");
-
-            art.removeClass("active");
-            business.removeClass("active");
-            scitech.removeClass("active");
-            general.removeClass("active");
-            trivia.removeClass("active");
-            sports.removeClass("active");
-            others.removeClass("active");
-        }
     }
+    check();
 });
 
 $(".delete").click(function (){
